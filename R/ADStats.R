8b473a27eb14024ec73884efdaf782e8930a01be
Anantajit Subrahmanya <anantajit.subrahmanya@gmail.com>
2023-03-21 11:17:29 -0500

    include test of normality

--- R/ADStats.R
+++ R/ADStats.R
@@ -8,10 +8,12 @@ LSM <- lsmeans(ADLME, ~ Group * Tercile)
 summary(pairs(LSM))
 
 # Analysis of Variance: Kruskal-Wallis Test
+# Test of Normality: Kolmogorov-Smirnov Test
 groups = unique(ADtable$Group)
 for (group in groups) {
 	group_data <- ADtable[ADtable$Group == group, ]
 	print(group)
 	print(kruskal.test(Centering ~ Tercile, data = group_data))
+	print(ks.test(group_data$InitialPitch, "pnorm"))
+	print(ks.test(group_data$EndingPitch, "pnorm"))
 }
-
