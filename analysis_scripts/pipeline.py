099b1c65c8cad7c562f3eb11a7ab41023f9d5a6c
Anantajit Subrahmanya <anantajit.subrahmanya@gmail.com>
2024-09-04 11:41:13 -0700

    Removed redundant code

--- analysis_scripts/pipeline.py
+++
@@ -1,9 +0,0 @@
-
-
-def pipeline(data, transforms):
-    for transform in transforms:
-        if type(transform) is tuple and len(transform) >= 2 and isinstance(transform[1], dict):
-            data = transform[0](data, **transform[1])
-        else:
-            data = transform(data)
-    return data
